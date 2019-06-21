e47fb83464d1bdfae74c9e9e074dbee21f20cf15
Georgi Tsvetanov <georgicvettanov@gmail.com>
2019-06-21 12:07:13 +0300

    final

--- Site Final/app.js
+++ Site Final/app.js
@@ -1,32 +1,24 @@
-// Write our JavaScript in here
 var pics = [
-  "imgs/1.jpg",
+  "imgs/1.png",
   "imgs/2.jpg",
   "imgs/3.jpg",
   "imgs/4.jpg",
   "imgs/5.jpg",
   "imgs/6.jpg",
-  "imgs/7.png",
+  "imgs/7.jpg",
   "imgs/8.jpg",
   "imgs/9.jpg",
   "imgs/10.jpg",
-  "imgs/11.jpg",
-  "imgs/12.jpg",
-  "imgs/13.ico",
-  "imgs/14.jpg",
-  "imgs/15.jpg",
-  "imgs/16.png",
-  "imgs/17.png",
-  "imgs/18.jpg",
-  "imgs/19.jpg",
+  "imgs/11.png",
+  "imgs/12.jpg"
 ];
 
 var btn = document.querySelector("button");
 var img = document.querySelector("img");
-var count = 15;
+var count = 0;
 
 btn.addEventListener("click", function () {
-  if(count === 19){
+  if(count === 12){
     count = 0;
   }
   img.src = pics[count]
